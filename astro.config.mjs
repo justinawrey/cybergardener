10bf0fa3b05162f17b756eb0c2b77607a75b542f
Justin Awrey <awreyjustin@gmail.com>
2023-09-08 11:32:56 +0200

    adding more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import deno from "@astrojs/deno";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), solidJs()],
   output: "server",
   adapter: deno()
 });
