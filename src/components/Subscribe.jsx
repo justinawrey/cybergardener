840f59bfe210468b7de3c6431c2465257d112f62
Justin Awrey <awreyjustin@gmail.com>
2023-09-08 12:18:51 +0200

    test

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,26 +1,17 @@
-function subscribe() {
-  const isDenoDeploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
-
-  let senderKey = isDenoDeploy
-    ? Deno.env.get("PUBLIC_SENDER_API_KEY")
-    : import.meta.env.PUBLIC_SENDER_API_KEY;
-
-  const url = new URL("https://api.sender.net/v2/subscribers");
-
-  let headers = {
-    Authorization: `Bearer ${senderKey}`,
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  };
-
-  fetch(url, {
-    method: "GET",
-    headers,
-  }).then((response) => console.log(response.json()));
+function subscribe(email) {
+  fetch("/api/subscribe", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+    }),
+  });
 }
 
 const CountingComponent = () => {
-  return <button onClick={subscribe}>get subscribers</button>;
+  return <button onClick={() => subscribe("justin")}>get subscribers</button>;
 };
 
 export default CountingComponent;
---
+++ src/pages/api/subscribe.js
@@ -0,0 +1,43 @@
+async function subscribe(email) {
+  const isDenoDeploy = "Deno" in globalThis;
+
+  let senderKey = isDenoDeploy
+    ? Deno.env.get("PUBLIC_SENDER_API_KEY")
+    : import.meta.env.PUBLIC_SENDER_API_KEY;
+
+  const url = new URL("https://api.sender.net/v2/subscribers");
+
+  let headers = {
+    Authorization: `Bearer ${senderKey}`,
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+
+  return fetch(url, {
+    method: "GET",
+    headers,
+  });
+}
+
+export async function POST({ request }) {
+  let { email } = await request.json();
+
+  if (!email) {
+    return new Response({
+      status: 400,
+    });
+  }
+
+  // if something went wrong with sender api, just send 500
+  let res = await subscribe(email);
+  if (!res.ok) {
+    return new Response({
+      status: 500,
+    });
+  }
+
+  // otherwise we good
+  return new Response({
+    status: 200,
+  });
+}
